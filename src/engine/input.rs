f27031e4876b3847f6a3527ac0eac2b2732bbd96
Panagiotis Koutsourakis <kutsurak@zarniwoop.org>
2018-03-12 11:53:20 +0100

    Allow PC to move diagonally

--- src/engine/input.rs
+++ src/engine/input.rs
@@ -1,5 +1,5 @@
 use tcod::input::Key;
-use tcod::input::KeyCode::{Enter, Escape, Up, Down, Right, Left};
+use tcod::input::KeyCode::*;
 
 pub enum Action {
     Empty,
@@ -11,11 +11,15 @@ pub enum Action {
 
 pub fn handle_input(keypress: Key) -> Action {
     match keypress {
-        Key { code: Escape, .. } => Action::Exit,
-        Key { code: Up, .. }     => Action::Move((0, -1)),
-        Key { code: Down, .. }   => Action::Move((0, 1)),
-        Key { code: Left, .. }   => Action::Move((-1, 0)),
-        Key { code: Right, .. }  => Action::Move((1, 0)),
+        Key { code: Escape, ..} => Action::Exit,
+        Key { code: Up, ..}        | Key { code: NumPad8, ..} => Action::Move((0, -1)),
+        Key { code: Down, ..}      | Key { code: NumPad2, ..} => Action::Move((0, 1)),
+        Key { code: Left, ..}      | Key { code: NumPad4, ..} => Action::Move((-1, 0)),
+        Key { code: Right, ..}     | Key { code: NumPad6, ..} => Action::Move((1, 0)),
+        Key { code: Home, ..}      | Key { code: NumPad7, ..} => Action::Move((-1, -1)),
+        Key { code: PageUp, ..}    | Key { code: NumPad9, ..} => Action::Move((1, -1)),
+        Key { code: End, ..}       | Key { code: NumPad1, ..} => Action::Move((-1, 1)),
+        Key { code: PageDown, ..}  | Key { code: NumPad3, ..} => Action::Move((1, 1)),
         Key { code: Enter, left_alt: true, .. }  => Action::Fullscreen,
         _ => Action::Empty,
     }
